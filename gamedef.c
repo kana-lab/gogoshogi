880fed4245af9649ee732eaae141c7747e3993dc
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-11 12:58:49 +0900

    change piece_ch

--- gamedef.c
+++ gamedef.c
@@ -82,7 +82,7 @@ void print_board_for_debug(Board *b) {
 
 #ifdef DEBUG_MODE
     static const char *piece_ch[] = {
-        "　",
+        "..",
         "歩", "角", "飛", "銀", "金", "王",
         "と", "馬", "龍", "全"
     };
