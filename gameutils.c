65e6e4db6e064eda0d5dfcf6cb772a5448c2a7c6
kana-lab <kirinokanatrust@gmail.com>
2021-12-12 15:13:55 +0900

    関数の大半をgamedef.cに移動した 今後もっと良い配置に調整していく予定

--- gameutils.c
+++ gameutils.c
@@ -2,6 +2,7 @@
 #define BOARD_UTILS
 
 #include "gamedef.c"
+#include "possible_actions.c"
 
 //
 // このモジュールでは、盤面Boardや指手Actionに関連する関数を定義する
@@ -10,90 +11,8 @@
 
 /************************************
  * 定義済み関数一覧
- *   - create_board
- *   - reverse_board
- *   - is_checked
- *   - is_checking
- *   - is_checkmate
- *   - update_board
- *   - reverse_action
- *   - get_all_actions
- *   - get_all_possible_actions
- *   - is_possible_action
  *   - is_threefold_repetition
  ************************************/
-
-Board create_board(int first_mover) {
-    // Board型の盤面を作って初期化し、それを戻り値として返す
-    // first_mover引数は先手を表し、USER か AI のいずれかである
-}
-
-void reverse_board(Board *b) {
-    // 盤面を反転させる
-    // b.board[5][5]の全要素に-1を掛けて180°回転する
-}
-
-int is_checked(Board *b) {
-    // 相手の王に対して、王手になっているか否かを判定する
-    // 王手であれば1、そうでなければ0を返す
-}
-
-int is_checking(Board *b) {
-    // 手番側が王手をしているかを判定する
-}
-
-int is_checkmate(Board *b) {
-    // 盤面bの相手の詰みを判定する (詰みなら1、そうでないなら0を返す)
-    // ステイルメイトは詰みの判定にしない事が望ましい
-}
-
-void update_board(Board *b, Action action) {
-    // 駒を動かして盤面を更新する
-    // 一切の反則手のチェックをしないので注意！
-    // インデックスの範囲のチェックぐらいはするかも (しない)
-
-    if (action.from_stock) {  // 持ち駒を打つ場合
-        b->board[action.to_x][action.to_y] = action.from_stock;
-        --b->next_stock[action.from_stock];
-    } else {  // 駒を動かす場合
-        int piece = b->board[action.from_x][action.from_y];
-        b->board[action.from_x][action.from_y] = EMPTY;  // 移動元を空にする
-
-        int gain = abs(b->board[action.to_x][action.to_y]);
-        if (gain != EMPTY) {  // 移動先に相手の駒がある場合、それを持ち駒に加える
-            gain = (gain > NARI) ? gain - NARI : gain;
-            ++b->next_stock[gain];
-        }
-
-        b->board[action.to_x][action.to_y] = piece;  // 駒を移動先に持っていく
-    }
-}
-
-void reverse_action(Action *action) {
-    // actionの表す動きを、盤面を180°回転させた時の新たな動きに変更する
-}
-
-
-int get_all_actions(Board *b, Action actions[250]) {
-    // 盤面bから可能な指手を全列挙し、actions配列に入れる
-    // 打ち歩詰めと王手放置は可能なものとする
-    // 二歩と行きどころのない駒は判定する？
-    // 多分配列の長さは250あれば足りると思いますが、適当に調整して下さい
-    // 戻り値はactionsに入れた要素の個数
-}
-
-int get_all_possible_actions(Board *b, Action actions[250]) {
-    // 盤面bから可能な指手を全列挙し、actions配列に入れる
-    // 打ち歩詰め、王手放置、二歩、行きどころのない駒の全てを判定し、
-    // 反則手を除いたものを返す
-    // ただし連続王手千日手は判定しない
-    // 戻り値はactionsに入れた要素の個数
-}
-
-int is_possible_action(Board *b, Action action) {
-    // actionがget_all_possible_actionに含まれるか否かを返す
-    // 含まれるなら1、そうでないなら0を返す
-}
 
 int is_threefold_repetition(Board history[], int history_len, Board *b) {
     // 盤面bは自分が手を打ち終わった直後であると見なし、盤面はその後回転していないものとする
