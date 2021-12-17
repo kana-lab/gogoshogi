5990408d4e3d65fdcb0674efe4a1a36defee660f
kana-lab <kirinokanatrust@gmail.com>
2021-12-17 16:40:56 +0900

    initial commit

---
+++ montecarlo.c
@@ -0,0 +1,68 @@
+#ifndef MONTECARLO
+#define MONTECARLO
+
+
+#include "gamedef.c"
+
+#define EXPANSION_THRESHOLD 10
+#define
+
+typedef struct tagNode {     // モンテカルロ法で探索する木の頂点を表す構造体
+    Board b;                 // 盤面
+    int is_checkmate;        // 何回も勝敗を判定するのは無駄なのでメモしておく
+    int visit_count;         // この頂点が訪問された回数
+    int win_count;           // このノード以下の葉ノードで自分が勝ちとなるものの個数
+    struct tagNode child[];  // 子ノードの配列
+    int child_len;           // 子ノードの配列の長さ
+} Node;
+
+
+Node *select(Node *root) {
+    // モンテカルロ法におけるselectionを行う
+    // rootの子ノードの中からQ値+コストが最大になるものを選び、そのアドレスを返す (UCTの場合)
+    // もし子ノードがなければNULLを返す
+    // もしQ値+コストが無限大となるものがあれば、それを返す
+}
+
+void expand(Node *root) {
+    // rootに新たな子ノードを1つ加える (本当に1つ？文献によって違う)
+    // 1つの場合の疑問点: 選び方はランダム？ それ以上新たに加えられる指手がない場合はスルー？
+    // 1つではなく全ての場合はrootは葉ノードである必要がある
+    // 全ての場合の疑問点: expansion後、各ノードのカウントはリセット？それともそのまま？
+    // とりあえず、expansionは考えられる全ての場合を展開することにし、カウンタはリセットしないとする
+    // なお、加えるNode構造体はcalloc関数により動的に確保する
+}
+
+int evaluate(Board b) {
+    // 盤面bからランダムにプレイアウトを行い勝敗を判定する
+    // 自分が勝った場合1を、相手が勝った場合0を返す
+}
+
+int monte_carlo_tree_search(Node *root) {
+    // モンテカルロ木探索を1回行い、勝った場合1を、負けた場合0を返す
+
+    ++root->visit_count;
+    int i_win;
+
+    Node *child = select(root);
+    if (child == NULL) {  // rootが葉ノードであった場合
+        if (root->is_checkmate) {
+            i_win = 0;
+        } else if (root->visit_count >= EXPANSION_THRESHOLD) {
+            expand(root);
+            --root->visit_count;
+            i_win = monte_carlo_tree_search(root);
+        } else {
+            i_win = evaluate(root->b);
+        }
+    } else {  // rootが内部ノードであった場合
+        int opponent_win = monte_carlo_tree_search(child);
+        i_win = (opponent_win) ? 0 : 1;
+    }
+
+    root->win_count += i_win;
+    return i_win;
+}
+
+
+#endif  /* MONTECARLO */
