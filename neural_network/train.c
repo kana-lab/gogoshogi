2b78babdd7f8eb80dd5908e4c55230c1403a87f6
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-20 20:14:59 +0900

    Update train.c

--- neural_network/train.c
+++ neural_network/train.c
@@ -2,11 +2,11 @@
 #include "neural_network/neural_network.c"
 #include "hash.c"
 
-#define INPUT_SIZE 535
+#define INPUT_SIZE 585
 #define TRAIN_SIZE 360000
 #define TEST_SIZE 96554
 #define DATASET "checkmates456554.txt"
-#define WEIGHTS_FILE "weights535.txt"
+#define WEIGHTS_FILE "weights585.txt"
 
 
 /*
@@ -14,7 +14,87 @@
 */
 
 
-void board_to_vector(Board *b, double vec[INPUT_SIZE]){
+static int move_matrix_x[MAX_PIECE_NUMBER + 1][8] = {
+        {},                           // EMPTY
+        {-1},                         // 歩
+        {-1, -1, 1,  1},              // 角1
+        {-1, 1,  0,  0},              // 飛1
+        {-1, -1, -1, 1, 1},           // 銀
+        {-1, -1, -1, 0, 0, 1},        // 金
+        {-1, -1, -1, 0, 0, 1, 1, 1},  // 王
+        {-1, -1, -1, 0, 0, 1},        // と
+        {-1, 1,  0,  0},              // 馬 - 角
+        {-1, -1, 1,  1},              // 龍 - 飛
+        {-1, -1, -1, 0, 0, 1}         // 全
+};
+
+static int move_matrix_y[MAX_PIECE_NUMBER + 1][8] = {
+        {},                            // EMPTY
+        {0},                           // 歩
+        {-1, 1, -1, 1},                // 角1
+        {0,  0, -1, 1},                // 飛1
+        {-1, 0, 1,  -1, 1},            // 銀
+        {-1, 0, 1,  -1, 1, 0},         // 金
+        {-1, 0, 1,  -1, 1, -1, 0, 1},  // 王
+        {-1, 0, 1,  -1, 1, 0},         // と
+        {0,  0, -1, 1},                // 馬 - 角
+        {-1, 1, -1, 1},                // 龍 - 飛
+        {-1, 0, 1,  -1, 1, 0},         // 全
+};
+
+static int move_length[MAX_PIECE_NUMBER + 1] = {0, 1, 4, 4, 5, 6, 8, 6, 4, 4, 6};
+
+void count_connections(const Board *b, double counts[5][5]) {
+    /*
+    各マスについて, ききのある駒の数を数える.
+    */
+
+    // 初期化する.
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++)
+            counts[i][j] = 0.0;
+    }
+    
+    // 数える.
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            int piece = b->board[i][j];
+            if (piece <= 0)
+                // 自分の駒がないとき
+                continue;
+            if (piece != KAKU && piece != HISHA) {
+                // 駒が角や飛ではないときに, 周囲に動く手を列挙する.
+                for (int k = 0; k < move_length[piece]; k++) {
+                    int x = i + move_matrix_x[piece][k];
+                    int y = j + move_matrix_y[piece][k];
+                    if (0 <= x && x < 5 && 0 <= y && y < 5){// && b->board[x][y] <= EMPTY) {
+                        counts[x][y] += 1.0;
+                    }
+                }
+            }
+            if (piece % NARI == KAKU || piece % NARI == HISHA) {
+                // 角, 馬, 飛, 龍が直線的に動く手を列挙する.
+                for (int k = 0; k < move_length[piece % NARI]; k++) {
+                    int x = i + move_matrix_x[piece % NARI][k];
+                    int y = j + move_matrix_y[piece % NARI][k];
+                    while (0 <= x && x < 5 && 0 <= y && y < 5){// && b->board[x][y] <= EMPTY) {
+                        counts[x][y] += 1.0;
+                        /*
+                        if (b->board[x][y] < EMPTY)
+                            // 相手の駒を取ったとき
+                            break;
+                        */
+                        x += move_matrix_x[piece % NARI][k];
+                        y += move_matrix_y[piece % NARI][k];
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void board_to_vector(const Board *b, double vec[INPUT_SIZE]){
     // 盤面を1次元のベクトルに変換する.
 
     // vecを初期化する.
@@ -31,6 +111,24 @@ void board_to_vector(Board *b, double vec[INPUT_SIZE]){
     for (int i = 0; i < 5; i++){
         vec[21*25 + i] = (double)b->next_stock[i+1];
         vec[21*25+5 + i] = (double)b->previous_stock[i+1];
+    }
+
+    // 自分の駒のききの数を入力する.
+    double counts[5][5];
+    count_connections(b, counts);
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++)
+            vec[21*25+10 + 5*i + j] = counts[i][j];
+    }
+
+    // 相手の駒のききの数を入力する.
+    // 座標が180度ずれているが, 気にしないことにする.
+    Board b_copy = *b;
+    reverse_board(&b_copy);
+    count_connections(&b_copy, counts);
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++)
+            vec[21*25+10+25 + 5*i + j] = counts[i][j];
     }
 }
 
@@ -96,7 +194,7 @@ int main(void){
     nn_fit(nn, X_train, y_train, 2*TRAIN_SIZE, X_test, y_test, TEST_SIZE, lr, epoch);
 
     // 重みの保存
-    nn_save_weights(nn, WEIGHTS_FILE);
+    //nn_save_weights(nn, WEIGHTS_FILE);
 
     return 0;
 }
