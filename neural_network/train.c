399d45fbdb02bc2cc8812388867336274b0af472
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-21 22:19:14 +0900

    Update train.c

--- neural_network/train.c
+++ neural_network/train.c
@@ -8,6 +8,8 @@
 #define DATASET "checkmates456554.txt"
 #define WEIGHTS_FILE "weights585.txt"
 
+#define LEN_ARRAY(a) ((sizeof(a)) / (sizeof(a[0])))
+
 
 /*
 ニューラルネットワークの学習を行う.
@@ -63,58 +65,81 @@ void mirror_board(Board *b) {
 }
 
 
-int main(void){
+void learn_dataset(NeuralNetwork *nn, char dataset[], int train_size, int test_size, char load_file[], char save_file[]){
+    // datasetを学習する.
+    // load_file == NULL のときは重みを読み込まない.
+    // save_file == NULL のときは重みを保存しない.
 
-    // モデルの準備
-    NeuralNetwork nn;
-    int depth = 3;
-    int sizes[4] = {INPUT_SIZE, 32, 32, 1};
-    nn_init(&nn, depth, sizes);
+    // 入出力サイズを取得する.
+    int input_size = nn->affine[0].n;
+    int output_size = nn->sigmoid.len;
 
     // データセットの準備
-    FILE *fp = fopen(DATASET, "r");
+    // 学習データは左右を反転させて2倍にする.
+
+    // メモリを確保する.
     double **X_train, **y_train, **X_test, **y_test;
-    X_train = malloc(2 * TRAIN_SIZE * sizeof(double *));
-    y_train = malloc(2 * TRAIN_SIZE * sizeof(double *));
-    X_test = malloc(TEST_SIZE * sizeof(double *));
-    y_test = malloc(TEST_SIZE * sizeof(double *));
-    for (int i = 0; i < 2 * TRAIN_SIZE; i++) {
-        X_train[i] = malloc(INPUT_SIZE * sizeof(double));
-        y_train[i] = malloc(1 * sizeof(double));
+    X_train = malloc(2 * train_size * sizeof(double *));
+    y_train = malloc(2 * train_size * sizeof(double *));
+    X_test = malloc(test_size * sizeof(double *));
+    y_test = malloc(test_size * sizeof(double *));
+    for (int i = 0; i < 2 * train_size; i++) {
+        X_train[i] = malloc(input_size * sizeof(double));
+        y_train[i] = malloc(output_size * sizeof(double));
     }
-    for (int i = 0; i < TEST_SIZE; i++) {
-        X_test[i] = malloc(INPUT_SIZE * sizeof(double));
-        y_test[i] = malloc(1 * sizeof(double));
+    for (int i = 0; i < test_size; i++) {
+        X_test[i] = malloc(input_size * sizeof(double));
+        y_test[i] = malloc(output_size * sizeof(double));
     }
-    for (int i = 0; i < TRAIN_SIZE + TEST_SIZE; i++) {
+
+    // 学習データを読み込む.
+    FILE *fp = fopen(dataset, "r");
+    for (int i = 0; i < train_size + test_size; i++) {
         Hash h;
         double ans;
         fscanf(fp, "%llu %llu %lf", &h.lower, &h.upper, &ans);
         Board b = decode(h);
-        if (i < TRAIN_SIZE) {
+        if (i < train_size) {
             board_to_vector(&b, X_train[i]);
             y_train[i][0] = ans;
             // Data Augmentation
             mirror_board(&b);
-            board_to_vector(&b, X_train[i + TRAIN_SIZE]);
-            y_train[i + TRAIN_SIZE][0] = ans;
+            board_to_vector(&b, X_train[i + train_size]);
+            y_train[i + train_size][0] = ans;
         } else {
-            board_to_vector(&b, X_test[i - TRAIN_SIZE]);
-            y_test[i - TRAIN_SIZE][0] = ans;
+            board_to_vector(&b, X_test[i - train_size]);
+            y_test[i - train_size][0] = ans;
         }
     }
     fclose(fp);
 
     // 重みの読み込み
-    //nn_load_weights(&nn, WEIGHTS_FILE);
+    if (load_file != NULL)
+        nn_load_weights(nn, load_file);
 
     // モデルの学習
     double lr = 0.001;
     int epoch = 5;
-    nn_fit(&nn, X_train, y_train, 2 * TRAIN_SIZE, X_test, y_test, TEST_SIZE, lr, epoch);
+    nn_fit(nn, X_train, y_train, 2 * train_size, X_test, y_test, test_size, lr, epoch);
 
     // 重みの保存
-    //nn_save_weights(&nn, WEIGHTS_FILE);
+    if (save_file != NULL)
+        nn_save_weights(nn, save_file);
+}
+
+
+int main(void){
+    // learn_datasetの使用例
+
+    // モデルの準備
+    NeuralNetwork nn;
+    int depth = 3;
+    int sizes[4] = {INPUT_SIZE, 32, 32, 1};
+    assert(LEN_ARRAY(sizes) - 1 == depth);
+    nn_init(&nn, depth, sizes);
+
+    // モデルを学習させる.
+    learn_dataset(&nn, DATASET, TRAIN_SIZE, TEST_SIZE, NULL, WEIGHTS_FILE);
 
     return 0;
 }
