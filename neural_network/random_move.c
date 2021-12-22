765d01ace0f90fbe4e3421ea59548cbed1454cbf
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-22 13:57:32 +0900

    Remove save_board

--- neural_network/random_move.c
+++ neural_network/random_move.c
@@ -4,10 +4,6 @@
 #include <stdio.h>
 
 
-#define HASH_MOD 9999991 // 10^7以下の最大素数
-#define DATASET "checkmates.txt"
-
-
 /*
 動かす駒がランダムなAIを実装した.
 */
@@ -60,29 +56,4 @@ Action random_move_ai(const Game *game) {
     Action all_actions[LEN_ACTIONS];
     int len_all_actions = get_all_actions_with_tfr(game, all_actions);
     return choose_random_action(all_actions, len_all_actions);
-}
-
-
-int get_hash_mod(const Hash *h) {
-    return (h->lower ^ h->upper) % HASH_MOD;
-}
-
-
-void save_board(Game *game, bool hash_table[HASH_MOD]) {
-    // hash_tableとhashが衝突しないときに, gameをdatasetに保存する.
-    FILE *fp = fopen(DATASET, "a");
-    Hash win_hash = reverse_hash(game->history[game->history_len - 2]);
-    Hash lose_hash = reverse_hash(game->history[game->history_len - 1]);
-    Board lose_board = decode(lose_hash);
-    if (is_checkmate(&lose_board)) {
-        if (hash_table[get_hash_mod(&win_hash)]) {
-            if (hash_table[get_hash_mod(&lose_hash)]) {
-                fprintf(fp, "%llu %llu 1.0\n", win_hash.lower, win_hash.upper);
-                fprintf(fp, "%llu %llu 0.0\n", lose_hash.lower, lose_hash.upper);
-                hash_table[get_hash_mod(&win_hash)] = false;
-                hash_table[get_hash_mod(&lose_hash)] = false;
-            }
-        }
-    }
-    fclose(fp);
 }
