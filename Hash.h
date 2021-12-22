b3bc8b6369351f0f45f3f0ad06b3f8de634fce46
kana-lab <kirinokanatrust@gmail.com>
2021-12-22 11:58:52 +0900

    initial commit

---
+++ Hash.h
@@ -0,0 +1,31 @@
+#ifndef HASH_H
+#define HASH_H
+
+
+#include <stdbool.h>
+#include "Board.h"
+
+
+/*********************************
+ * Hashクラスの定義
+ *********************************/
+
+typedef struct {               // 96bitのハッシュを入れる構造体
+    unsigned long long lower;  // lowerには下位の64bitを入れる
+    unsigned long long upper;  // upperには上位の32bitを入れる
+} Hash;
+
+
+/*********************************
+ * Hashクラスのメソッド
+ *********************************/
+
+Hash encode(const Board *b);
+
+Board decode(Hash h);
+
+Hash reverse_hash(Hash h);
+
+bool hash_equal(Hash h1, Hash h2);
+
+#endif  /* HASH_H */
