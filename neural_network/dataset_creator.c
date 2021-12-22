612f25945b9e6228f96f4a593d152f124b69d072
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-22 17:58:44 +0900

    Debug save_checkmate_board

--- neural_network/dataset_creator.c
+++ neural_network/dataset_creator.c
@@ -25,12 +25,12 @@ void save_checkmate_board(Game *game, bool hash_table[HASH_MOD]) {
     Hash lose_hash = reverse_hash(game->history[game->history_len - 1]);
     Board lose_board = decode(lose_hash);
     if (is_checkmate(&lose_board)) {
-        if (hash_table[get_hash_mod(&win_hash)]) {
-            if (hash_table[get_hash_mod(&lose_hash)]) {
+        if (!hash_table[get_hash_mod(&win_hash)]) {
+            if (!hash_table[get_hash_mod(&lose_hash)]) {
                 fprintf(fp, "%llu %llu 1.0\n", win_hash.lower, win_hash.upper);
                 fprintf(fp, "%llu %llu 0.0\n", lose_hash.lower, lose_hash.upper);
-                hash_table[get_hash_mod(&win_hash)] = false;
-                hash_table[get_hash_mod(&lose_hash)] = false;
+                hash_table[get_hash_mod(&win_hash)] = true;
+                hash_table[get_hash_mod(&lose_hash)] = true;
             }
         }
     }
