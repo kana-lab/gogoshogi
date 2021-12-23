7a399878f1028f506b72f31d837c82410d511dce
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-23 13:33:46 +0900

    minor change

--- neural_network/nn_main.c
+++ neural_network/nn_main.c
@@ -4,14 +4,10 @@
 
 
 int main(void){
-
-    // バグにより実行できません.
     
     // プレイヤーを初期化する.
     NNAI first = create_read1_ai("nn_585_32x2_0.txt");
     NNAI second = create_read1_ai("nn_585_32x2_0.txt");
-    
-    //
 
     // データセットを作成する.
     create_dataset((PlayerInterface *) &first, (PlayerInterface *) &second, DATASET, 1, "self_match");
