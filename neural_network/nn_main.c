2c1b5898a1b567a074b1dcbb26633852c41a2d9b
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-24 21:48:59 +0900

    minor change

--- neural_network/nn_main.c
+++ neural_network/nn_main.c
@@ -1,13 +1,14 @@
 #include "dataset_creator.c"
 
-#define MODEL_FILE "nn_360_128x2_1.txt"
+#define MODEL_FILE "nn_361_32x2_0.txt"
+#define DATASET "checkmates456554.txt"
 
 
 int main(void){
     
-    //learn_dataset(DATASET, 360000, 96554, NULL, MODEL_FILE);
-    //self_match_learning(MODEL_FILE, 5);
-    //learn_dataset(DATASET, 0, 400000, MODEL_FILE, NULL);
+    learn_dataset(DATASET, 360000, 96554, NULL, MODEL_FILE);
+    //self_match_learning(MODEL_FILE, 1);
+    //learn_dataset(DATASET, 0, 100000, MODEL_FILE, NULL);
 
     return 0;
 }
