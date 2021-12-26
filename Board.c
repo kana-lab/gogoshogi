cded31ca915764cb97cb978bb759a816fd4670e7
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-26 13:55:50 +0900

    Delete count_connections

--- Board.c
+++ Board.c
@@ -517,58 +517,6 @@ int get_useful_actions(const Board *b, Action actions[LEN_ACTIONS]) {
 }
 
 
-void count_connections(const Board *b, double counts[5][5]) {
-    /*
-    各マスについて, ききのある駒の数を数える.
-    評価関数の入力に用いる.
-    */
-
-    // 初期化する.
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++)
-            counts[i][j] = 0.0;
-    }
-
-    // 数える.
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            int piece = b->board[i][j];
-            if (piece <= 0)
-                // 自分の駒がないとき
-                continue;
-            if (piece != KAKU && piece != HISHA) {
-                // 駒が角や飛ではないときに, 周囲に動く手を列挙する.
-                for (int k = 0; k < move_length[piece]; k++) {
-                    int x = i + move_matrix_x[piece][k];
-                    int y = j + move_matrix_y[piece][k];
-                    if (0 <= x && x < 5 && 0 <= y && y < 5) {// && b->board[x][y] <= EMPTY) {
-                        counts[x][y] += 1.0;
-                    }
-                }
-            }
-            if (piece % NARI == KAKU || piece % NARI == HISHA) {
-                // 角, 馬, 飛, 龍が直線的に動く手を列挙する.
-                for (int k = 0; k < move_length[piece % NARI]; k++) {
-                    int x = i + move_matrix_x[piece % NARI][k];
-                    int y = j + move_matrix_y[piece % NARI][k];
-                    while (0 <= x && x < 5 && 0 <= y && y < 5) {// && b->board[x][y] <= EMPTY) {
-                        counts[x][y] += 1.0;
-                        // 駒を飛び越えてききがあることにする.
-                        /*
-                        if (b->board[x][y] < EMPTY)
-                            // 相手の駒を取ったとき
-                            break;
-                        */
-                        x += move_matrix_x[piece % NARI][k];
-                        y += move_matrix_y[piece % NARI][k];
-                    }
-                }
-            }
-        }
-    }
-}
-
-
 void piece_moves_to_vector(const Board *b, double vec[], int start_index) {
     /*
     各マスについて, ききのある駒の数を数える.
