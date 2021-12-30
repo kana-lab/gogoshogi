7ddfa3e8b70ca8bba3426b4de9c2273a4e332c4c
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-30 16:27:44 +0900

    Add stop_watch

--- gamedef.c
+++ gamedef.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
 #include "gamedef.h"
 
 
@@ -16,4 +17,20 @@ void debug_print(const char *msg, ...) {
     fflush(stdin);
     va_end(ap);
 #endif  /* DEBUG_MODE */
+}
+
+
+double stop_watch(struct timespec start_time, struct timespec end_time) {
+    // 経過時間を秒単位で返す.
+    /* 使用例
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    // 2秒間処理を止める.
+    sleep(2);
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    printf("%lf\n", stop_watch(start_time, end_time)); // 2.0程度の値が出力される.
+    */
+    long long int sec = end_time.tv_sec - start_time.tv_sec;
+    long long int nsec = end_time.tv_nsec - start_time.tv_nsec;
+    return (double)sec + (double)nsec / (1000 * 1000 * 1000);
 }
