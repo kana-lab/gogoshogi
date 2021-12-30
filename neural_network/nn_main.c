896abb3379daab3e205c76bd724d04390a1281bf
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-30 11:59:17 +0900

    minor change

--- neural_network/nn_main.c
+++ neural_network/nn_main.c
@@ -1,15 +1,18 @@
 #include "dataset_creator.c"
+#include "league_match.c"
 
-#define MODEL_FILE "nn_64x3_0.txt"
+#define MODEL_FILE "nn_128x2_64x2_32x2_1.txt"
 #define DATASET "checkmates456554.txt"
 
 
 int main(void){
     
     //learn_dataset(DATASET, 360000, 96554, NULL, MODEL_FILE);
-    //srand(0);
-    //self_match_learning(MODEL_FILE, 100);
+    //srand(1);
+    //self_match_learning(MODEL_FILE, 500);
     //learn_dataset(DATASET, 0, 100000, MODEL_FILE, NULL);
+
+    league_match();
 
     return 0;
 }
