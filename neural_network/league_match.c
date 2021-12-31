dc3f011e1e716342789eff3346eb96b6a3f0f3e4
eijirou <65397026+pekingenjin@users.noreply.github.com>
2021-12-31 11:22:19 +0900

    Add results

--- neural_network/league_match.c
+++ neural_network/league_match.c
@@ -7,6 +7,22 @@
 
 /*
 AI同士を対戦させ, 結果を出力する.
+
+このバージョンの出力
+
+Number 1: 32x2 minimax AI
+Number 2: 64x2 minimax AI
+Number 3: 128x2_64x2_32x2 minimax AI
+Number 4: 128x2_64x2_32x2 minimax AI 2
+
+League Match Results
+
+○×○×
+○○○×
+○○○×
+××○○
+
+(i行目には Number i が先手のときの結果が表示されています.)
 */
 
 
@@ -17,17 +33,17 @@ void league_match(void) {
     NNAI players[PLAYER];
     char *names[PLAYER];
 
-    players[0] = create_read1_ai("nn_32x2_1.txt");
-    names[0] = "32x2 read 1 depth AI";
+    players[0] = create_minimax_ai("nn_32x2_1.txt");
+    names[0] = "32x2 minimax AI";
+    
+    players[1] = create_minimax_ai("nn_64x2_1.txt");
+    names[1] = "64x2 minimax AI";
 
-    players[1] = create_read1_ai("nn_64x2_1.txt");
-    names[1] = "64x2 read 1 depth AI";
+    players[2] = create_minimax_ai("nn_128x2_64x2_32x2_1.txt");
+    names[2] = "128x2_64x2_32x2 minimax AI";
 
-    players[2] = create_read1_ai("nn_128x2_64x2_32x2_1.txt");
-    names[2] = "128x2_64x2_32x2 read 1 depth AI";
-
-    players[3] = create_minimax_ai("nn_128x2_64x2_32x2_1.txt");
-    names[3] = "128x2_64x2_32x2 minimax AI";
+    players[3] = create_minimax_ai("nn_128x2_64x2_32x2_2.txt");
+    names[3] = "128x2_64x2_32x2 minimax AI 2";
 
     // Player同士を対戦させる.
 
