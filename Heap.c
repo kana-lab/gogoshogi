18ab695655f6a4df8061b38016544447fc507e71
kumoryo9 <63239647+kumoryo9@users.noreply.github.com>
2022-01-04 14:34:13 +0900

    Update Heap.c

--- Heap.c
+++ Heap.c
@@ -20,11 +20,29 @@ static void heap_swap(Heap *heap, unsigned int index1, unsigned int index2) {
  * bubble_upでは、泡が親よりも小さい間ずっと浮上させ続ける
  * bubble_indexは、heap中の泡の位置を表す
  */
-static void bubble_up(Heap *heap, unsigned int bubble_index);
+static void bubble_up(Heap *heap, unsigned int bubble_index) {
+    while (bubble_index > 0 && heap->q[bubble_index]->value < heap->q[(bubble_index-1)/2]->value) {
+        heap_swap(heap, bubble_index, (bubble_index-1)/2);
+        bubble_index = (bubble_index-1) / 2;
+    }
+}
 
 
 /**
  * bubble_downでは泡が子よりも大きい間ずっと沈ませ続ける
  * bubble_indexは、heap中の泡の位置を表す
  */
-static void bubble_down(Heap *heap, unsigned int bubble_index);
+static void bubble_down(Heap *heap, unsigned int bubble_index) {
+    while (2*bubble_index + 2 < heap->current_size) {
+        unsigned int min_index;
+        if(heap->q[bubble_index*2+1]->value <= heap->q[bubble_index*2+2]->value) {
+            min_index = bubble_index * 2 + 1;
+        } else {
+            min_index = bubble_index * 2 + 2;
+        }
+        if(heap->q[bubble_index]->value > heap->q[min_index]->value) {
+            heap_swap(heap, bubble_index, min_index);
+            bubble_index = min_index;
+        } else break;
+    }
+}
